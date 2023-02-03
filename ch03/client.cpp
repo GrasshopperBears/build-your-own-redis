c5ffea69885998f4f83f81a0ec59d1b34bbf5daa
GrasshopperBears <starday456@kaist.ac.kr>
2023-02-04 00:42:20 +0900

    separate common client build logic

--- ch03/client.cpp
+++ ch03/client.cpp
@@ -2,31 +2,17 @@
 
 int main(int argc, char* argv[])
 {
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (fd < 0) {
-    cout << "Error on socket call";
-    exit(1);
-  }
+    int fd = initialize_client();
 
-  sockaddr_in addr = {};
-  addr.sin_family = AF_INET;
-  addr.sin_port = ntohs(8080);
-  addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);
+    char msg[] = "hello server";
+    write(fd, msg, strlen(msg));
 
-  if (connect(fd, (const struct sockaddr*)&addr, sizeof(addr))) {
-    cout << "Error on connect call";
-    exit(1);
-  }
+    char read_buf[MAX_MSG] = {};
+    if (read(fd, read_buf, sizeof(read_buf) - 1) < 0) {
+        cout << "Error on read call\n";
+        exit(1);
+    }
 
-  char msg[] = "hello server";
-  write(fd, msg, strlen(msg));
-
-  char read_buf[64] = {};
-  if (read(fd, read_buf, sizeof(read_buf) - 1) < 0) {
-    cout << "Error on read call";
-    exit(1);
-  }
-
-  printf("Data from server: %s\n", read_buf);
-  close(fd);
+    printf("Data from server: %s\n", read_buf);
+    close(fd);
 }
